f210e5855ff1004c24a5c2439accb3e7deb9dc6a
antman666 <945360554@qq.com>
2021-08-04 14:03:34 +0800

    update something

--- st.h
+++ st.h
@@ -22,36 +22,36 @@
 #define IS_TRUECOL(x)		(1 << 24 & (x))
 
 enum glyph_attribute {
-	ATTR_NULL       = 0,
-	ATTR_BOLD       = 1 << 0,
-	ATTR_FAINT      = 1 << 1,
-	ATTR_ITALIC     = 1 << 2,
-	ATTR_UNDERLINE  = 1 << 3,
-	ATTR_BLINK      = 1 << 4,
-	ATTR_REVERSE    = 1 << 5,
-	ATTR_INVISIBLE  = 1 << 6,
-	ATTR_STRUCK     = 1 << 7,
-	ATTR_WRAP       = 1 << 8,
-	ATTR_WIDE       = 1 << 9,
-	ATTR_WDUMMY     = 1 << 10,
-	ATTR_LIGA       = 1 << 11,
-	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+    ATTR_NULL       = 0,
+    ATTR_BOLD       = 1 << 0,
+    ATTR_FAINT      = 1 << 1,
+    ATTR_ITALIC     = 1 << 2,
+    ATTR_UNDERLINE  = 1 << 3,
+    ATTR_BLINK      = 1 << 4,
+    ATTR_REVERSE    = 1 << 5,
+    ATTR_INVISIBLE  = 1 << 6,
+    ATTR_STRUCK     = 1 << 7,
+    ATTR_WRAP       = 1 << 8,
+    ATTR_WIDE       = 1 << 9,
+    ATTR_WDUMMY     = 1 << 10,
+    ATTR_LIGA       = 1 << 11,
+    ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
 };
 
 enum selection_mode {
-	SEL_IDLE = 0,
-	SEL_EMPTY = 1,
-	SEL_READY = 2
+    SEL_IDLE = 0,
+    SEL_EMPTY = 1,
+    SEL_READY = 2
 };
 
 enum selection_type {
-	SEL_REGULAR = 1,
-	SEL_RECTANGULAR = 2
+    SEL_REGULAR = 1,
+    SEL_RECTANGULAR = 2
 };
 
 enum selection_snap {
-	SNAP_WORD = 1,
-	SNAP_LINE = 2
+    SNAP_WORD = 1,
+    SNAP_LINE = 2
 };
 
 typedef unsigned char uchar;
@@ -63,20 +63,20 @@ typedef uint_least32_t Rune;
 
 #define Glyph Glyph_
 typedef struct {
-	Rune u;           /* character code */
-	ushort mode;      /* attribute flags */
-	uint32_t fg;      /* foreground  */
-	uint32_t bg;      /* background  */
+    Rune u;           /* character code */
+    ushort mode;      /* attribute flags */
+    uint32_t fg;      /* foreground  */
+    uint32_t bg;      /* background  */
 } Glyph;
 
 typedef Glyph *Line;
 
 typedef union {
-	int i;
-	uint ui;
-	float f;
-	const void *v;
-	const char *s;
+    int i;
+    uint ui;
+    float f;
+    const void *v;
+    const char *s;
 } Arg;
 
 void die(const char *, ...);
