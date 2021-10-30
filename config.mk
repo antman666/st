fc9dda71c1977d9466fa8cc5829ec514787a24bf
antman666 <945360554@qq.com>
2021-10-30 19:56:08 +0800

    add jemalloc support

--- config.mk
+++ config.mk
@@ -24,8 +24,8 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS   = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) -Ofast -march=native -mtune=native -fomit-frame-pointer
-STLDFLAGS  = $(LIBS) $(LDFLAGS) -Ofast
+STCFLAGS   = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) -Ofast -march=native -mtune=native -fomit-frame-pointer -fno-common
+STLDFLAGS  = $(LIBS) $(LDFLAGS) -Ofast -ljemalloc
 
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
